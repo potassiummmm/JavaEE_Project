48ac328ce1aea326bfaf268e3ec0169a46172307
plusone <zhuozhengyi@icloud.com>
2020-12-19 16:15:08 +0800

    Add paths to /about, /contact; header modified

--- src/main/java/com/tongji/springbootdemo/controller/IndexController.java
+++ src/main/java/com/tongji/springbootdemo/controller/IndexController.java
@@ -1,9 +1,19 @@
 package com.tongji.springbootdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class IndexController {
 
+    @RequestMapping("/about")
+    public String about(Model model, HttpSession session) {
+        return "about";
+    }
+
+    @RequestMapping("/contact")
+    public String contact(Model model, HttpSession session) { return "contact"; }
 }
