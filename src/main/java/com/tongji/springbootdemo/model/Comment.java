394ff6e98b18e2a217e2c1f6b2cb56f34acd411a
ZhichenRen <1337795211@qq.com>
2020-12-20 22:43:15 +0800

    Create Comment Class

---
+++ src/main/java/com/tongji/springbootdemo/model/Comment.java
@@ -0,0 +1,17 @@
+package com.tongji.springbootdemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    private Integer blogId;
+    private String content;
+    private Integer nickname;
+    private Date date;
+}
