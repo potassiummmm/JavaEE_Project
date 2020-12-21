1b8ee8411deb33727b6d6465ce1cbe64692d49d0
ZhichenRen <1337795211@qq.com>
2020-12-21 19:03:18 +0800

    Post function

--- src/main/java/com/tongji/springbootdemo/controller/PostController.java
+++ src/main/java/com/tongji/springbootdemo/controller/PostController.java
@@ -1,7 +1,10 @@
 package com.tongji.springbootdemo.controller;
 
 import com.tongji.springbootdemo.mapper.BlogMapper;
+import com.tongji.springbootdemo.mapper.CommentMapper;
+import com.tongji.springbootdemo.mapper.UserMapper;
 import com.tongji.springbootdemo.model.Blog;
+import com.tongji.springbootdemo.model.User;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.sql.Date;
 import java.util.Collection;
 
 @Controller
@@ -21,6 +25,11 @@ public class PostController {
     @Autowired
     private BlogMapper blogMapper;
 
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @RequestMapping
     public String post(Model model) {
@@ -30,8 +39,8 @@ public class PostController {
         return "post";
     }
 
-    @RequestMapping("/{id}")
-    public String post(Model model, @PathVariable("id") Integer id ){
+    @RequestMapping("/{blogId}")
+    public String post(Model model, @PathVariable("blogId") Integer blogId ){
         //TODO
 //        Blog b = blogMapper.getBlogById(id);
 //        if (b == null)
@@ -41,16 +50,20 @@ public class PostController {
         return "post";
     }
 
-    @RequestMapping("/comment")
-    public String addComment(@RequestParam("commentContent") String comment){
+    @RequestMapping("/sendComment/{blogId}/{authorEmail}")
+    public String addComment(@RequestParam("commentContent") String comment, @PathVariable("blogId") Integer blogId, @PathVariable("authorEmail") String authorEmail){
         //TODO: Add database service here
-        return "post";
+        Date date = new Date(System.currentTimeMillis());
+        commentMapper.addComment(blogId, comment, userMapper.findByEmail(authorEmail).get(0).getUserId(), date);
+        return "redirect:/post/{blogId}";
     }
 
-    @RequestMapping("/sendBlog/{author}")
-    public String sendBlog(@RequestParam("blogTitle") String title, @RequestParam("blogContent") String content, @PathVariable("author") String author){
+    @RequestMapping("/sendBlog/{authorEmail}")
+    public String sendBlog(@RequestParam("blogTitle") String title, @RequestParam("blogContent") String content, @PathVariable("authorEmail") String authorEmail){
         //TODO: Add database service, use date.toString() to get date string(see main method in Blog.java)
-
-        return "post";
+        User author = userMapper.findByEmail(authorEmail).get(0);
+        Date date = new Date(System.currentTimeMillis());
+        blogMapper.addBlog(author.getUserId(),blogMapper.findByAuthor(author.getUserId()).size()+1,title,content, author.getNickname(), 0,0,date);
+        return "redirect:/post/1";
     }
 }
