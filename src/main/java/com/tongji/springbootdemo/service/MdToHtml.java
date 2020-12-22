084c90c00bc3a52ea4a04b149c6e0280012568f0
plusone <zhuozhengyi@icloud.com>
2020-12-23 01:26:28 +0800

    Last fix on md2html.

--- src/main/java/com/tongji/springbootdemo/service/MdToHtml.java
+++ src/main/java/com/tongji/springbootdemo/service/MdToHtml.java
@@ -1,35 +1,32 @@
 package com.tongji.springbootdemo.service;
 
 import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.html2md.converter.FlexmarkHtmlConverter;
+import com.vladsch.flexmark.html2md.converter.HtmlConverterCoreNodeRendererFactory;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.ast.Node;
 import com.vladsch.flexmark.util.data.MutableDataSet;
 
+import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class MdToHtml {
     public static String convert(String md) {
-        String escapeMd = md.replace("&", "&amp;")
-                            .replace("<", "&lt;")
-                            .replace(">", "&gt;");
-        System.out.println(escapeMd);
-
         MutableDataSet options = new MutableDataSet();
 
         Parser parser = Parser.builder(options).build();
         HtmlRenderer renderer = HtmlRenderer.builder(options).build();
 
-        // You can re-use parser and renderer instances
-        Node document = parser.parse(escapeMd);
-        String htmlOutPut = renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
-        htmlOutPut = htmlOutPut.replace("&amp;", "&")
-                               .replace("&lt;", "<")
-                               .replace("&gt;", ">");
+        Node document = parser.parse(md);
+        String htmlOutPut = renderer.render(document);
+
+        Pattern p = Pattern.compile("<\\s*input[\\S\\s]*?>|<\\s*form[\\S\\s]*?>|<\\s*script[\\S\\s]*?>|<\\s*button[\\S\\s]*>|<\\s*iframe[\\S\\s]*>|<\\s*nav[\\S\\s]*>|<\\s*meta[\\S\\s]*>");
+        Matcher m = p.matcher(htmlOutPut);
+
+        htmlOutPut = m.replaceAll("&lt;some tag&gt;");
         return htmlOutPut;
     }
 
-    public static void main(String[] args) {
-        String aa = "&lt;";
-        String bb = MdToHtml.convert(aa);
-        System.out.println(bb);
-    }
 }
